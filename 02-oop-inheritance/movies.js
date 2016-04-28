e80f32cdc3676c9a6a427cdfbb7dd271507484c3
mpintener <mpintener@gmail.com>
2016-04-27 21:17:26 -0300

    Exercise 5 done

--- 02-oop-inheritance/movies.js
+++ 02-oop-inheritance/movies.js
@@ -1,3 +1,49 @@
-/**
- * Created by Martina Pintener on 27/04/2016.
- */
+class EventEmitter {
+  constructor(){
+    this.events = {
+
+    }
+  }
+
+  on(event, callback){
+    this.events[event] = callback
+  }
+
+  emit(event){
+    this.events[event](event)
+  }
+
+  off(event){
+    delete this.events[event]
+  }
+}
+
+class Movie  extends EventEmitter {
+  constructor(title, year, duration) {
+    super()
+    this.title = title
+    this.year = year
+    this.duration  = duration
+  }
+  play(){
+    this.emit('play')
+  }
+  pause(){
+    this.emit('pause')
+  }
+  resume(){
+    this.emit('resume')
+  }
+}
+
+class Logger {
+  constructor(){}
+  log(info){
+    console.log('output: The' + info + 'event has been emitted');
+  }
+}
+
+let movie1 = new Movie('127 Hours', 2010, '120 min');
+let logger = new Logger();
+movie1.on('play', logger.log);
+movie1.play();
